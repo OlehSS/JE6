3879d43684d0cb2cb9b1066a3bf067061080a89f
OlehSS <133905314+OlehSS@users.noreply.github.com>
2023-09-09 10:21:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// Задание 1:
+
+// console.log('Старт');
+
+// for(let i = 1; i < 10; i++){
+//     console.log(i);
+// }
+
+// console.log('Финиш');
+
+// Задание 2:
+
+// for(let i = 10; i < 100; i++){
+//     if(i % 3 == 0){
+//         if(i % 5 == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+
+// Задание 3:
+
+// let array = [2, 45, 378, 234, 234, 14, 9, 5, 16]
+
+// for(let i = 0; i < array.length; i++){
+//     if(array[i] % 2 != 0){
+//         console.log(array[i]);
+//     }
+// }
+
+// Задание 4:
+
+// let array = [2, 45, 378, 234, 234, 14, 9, 5, 16, 23, 2, 3, 5]
+
+// for(let i = 1; i < array.length; i++){
+//     if(i % 3 == 0){
+//         console.log(array[i]);
+//     }
+// }
